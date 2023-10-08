049da4815f1930a337ec699f795728e646a48162
Sweathkumar <sweathkumarpersonal@gmail.com>
2023-10-08 09:24:55 +0530

    v-08.10.2023.2 (Without Errors)

--- src/main/java/com/sweath/smarthomeautomation/entities/Device.java
+++ src/main/java/com/sweath/smarthomeautomation/entities/Device.java
@@ -33,5 +33,13 @@ public class Device {
         this.type = type;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     // Other methods, if needed
 }
