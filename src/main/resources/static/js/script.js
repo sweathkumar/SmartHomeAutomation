4179ce17db87ac2a25b400ec9351f2c838918602
Sweathkumar <sweathkumarpersonal@gmail.com>
2023-10-11 14:07:24 +0530

    Version - 11.10.2023.3.5.3 USER_INTERFACE UPGRADE

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,69 +1,16 @@
-// Define button click events to control devices
-document.getElementById('living-room-light').addEventListener('click', function () {
-    // Code to toggle living room light
-    alert('Living room light switched');
+// Add functionality to toggle device buttons
+document.querySelectorAll('.control').forEach(button => {
+    button.addEventListener('click', function () {
+        const newLabel = this.textContent === 'Turn On' ? 'Turn Off' : 'Turn On';
+        this.textContent = newLabel;
+    });
 });
-
-// ... (The existing JavaScript code remains the same)
-
-// Update thermostat controls to handle slider input
-document.getElementById('living-room-thermostat').addEventListener('input', function () {
-    const temperature = this.value;
-    // Handle temperature adjustment logic for the living room thermostat
-    // You can update your smart home system here
-    alert(`Living Room Thermostat set to ${temperature}°C`);
-});
-
-
-document.getElementById('bedroom-light').addEventListener('click', function () {
-    // Code to toggle bedroom light
-    alert('Bedroom light switched');
-});
-
-// Add functionality to add a new device
-document.getElementById('device-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const deviceName = document.getElementById('device-name').value;
-    const deviceType = document.getElementById('device-type').value;
-
-    // Add the new device to the UI and your system
-    addNewDevice(deviceName, deviceType);
-    alert(`Added new device: ${deviceName} - ${deviceType}`);
-});
-
-function addNewDevice(name, type) {
-    // Create a new device element and append it to the UI
-    const newDevice = document.createElement('div');
-    newDevice.className = 'device';
-    newDevice.innerHTML = `
-        <img src="${getDeviceIcon(type)}" alt="${type}">
-        <p>${name}</p>
-        <button class="control">Control</button>
-    `;
-
-    document.querySelector('.room').appendChild(newDevice);
-}
-
-function getDeviceIcon(type) {
-    // Define icons for different device types
-    switch (type) {
-        case 'Light':
-            return 'light-icon.png';
-        case 'Thermostat':
-            return 'thermostat-icon.png';
-        default:
-            return 'default-icon.png'; // You can add a default icon
-    }
-}
-
-// ... (The existing JavaScript code remains the same)
 
 // Add functionality to add a new room
 document.getElementById('add-room-button').addEventListener('click', function () {
     const roomName = document.getElementById('room-name').value;
-    // Add the new room to the UI and your system
+    // Add the new room to the UI
     addNewRoom(roomName);
-    alert(`Added new room: ${roomName}`);
 });
 
 function addNewRoom(name) {
@@ -82,9 +29,8 @@ document.getElementById('add-device-button').addEventListener('click', function 
     const deviceName = document.getElementById('device-name').value;
     const deviceType = document.getElementById('device-type').value;
     const selectedRoomId = document.getElementById('room-select').value;
-    // Add the new device to the UI and your system
+    // Add the new device to the UI
     addNewDevice(selectedRoomId, deviceName, deviceType);
-    alert(`Added new device: ${deviceName} - ${deviceType}`);
 });
 
 function addNewDevice(roomId, name, type) {
@@ -95,33 +41,18 @@ function addNewDevice(roomId, name, type) {
     newDevice.innerHTML = `
         <img src="${getDeviceIcon(type)}" alt="${type}">
         <p>${name}</p>
-        <button class="control">Control</button>
+        <button class="control">Turn On</button>
     `;
     deviceContainer.appendChild(newDevice);
 }
-// ... (The existing JavaScript code remains the same)
 
-// Add functionality to toggle device buttons
-document.querySelectorAll('.control').forEach(button => {
-    button.addEventListener('click', function () {
-        const deviceName = this.previousElementSibling.textContent;
-        const newLabel = this.textContent === 'Turn On' ? 'Turn Off' : 'Turn On';
-        this.textContent = newLabel;
-        alert(`${deviceName} has been switched ${newLabel}`);
-    });
-});
-
-// ... (The existing JavaScript code remains the same)
-
-// Add functionality to add a new device
-document.getElementById('add-device-button').addEventListener('click', function () {
-    const deviceName = document.getElementById('device-name').value;
-    const deviceType = document.getElementById('device-type').value;
-    const selectedRoomId = document.getElementById('room-select').value;
-    // Add the new device to the UI and your system
-    addNewDevice(selectedRoomId, deviceName, deviceType);
-    alert(`Added new device: ${deviceName} - ${deviceType}`);
-});
-
-// ... (The existing JavaScript code remains the same)
-
+function getDeviceIcon(type) {
+    switch (type) {
+        case 'Light':
+            return 'light-icon.png';
+        case 'Thermostat':
+            return 'thermostat-icon.png';
+        default:
+            return 'default-icon.png';
+    }
+}
